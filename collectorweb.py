3020e7d985da4d5077930e6cf17dbdc4e2d4c884
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2010-06-05 13:00:13 -0500

    added util.py with its ThreadedDataStore object, converted collectorweb.py to use that object, and refactored collectorweb.py to behave nicely and look better.

--- collectorweb.py
+++ collectorweb.py
@@ -1,9 +1,13 @@
 import flask
-from flask import Flask
-
 import time
 
+from flask import Flask
+from flask import g
+
+import util
+
 app = Flask(__name__)
+glob = util.ThreadedDataStore()
 
 @app.route("/")
 def hello():
@@ -12,38 +16,43 @@ def hello():
 @app.route("/start_record")
 def start_record():
     # save the time we started recording for permanence
-    with open("start_record_time.save", 'w') as f:
-        start_time = time.time()
-        f.write(start_time)
-    
-    return "Start time is set to %d." % (start_time,)
+    with glob:
+        glob["start_time"] = time.time()
+        
+        # make the time prettier
+        string_time = time.asctime(time.localtime(glob["start_time"]))
+        
+        return "Start time is set to: %s" % string_time
 
 @app.route("/stop_record")
 def stop_record():
-    return "Not implemented."
+    raise NotImplementedError("Method not implemented yet.")
 
 @app.route("/get_elapsed_time")
 def get_elapsed_time():
-    elapsed_time = time.time() - flask.session['start_time']
-    return "%d time has elapsed" % (elapsed_time,)
+    with glob:
+        if "start_time" in glob:
+            elapsed_time = time.time() - glob["start_time"]
+        else:
+            return "No start time has been specified."
+    
+    return "%d seconds have elapsed." % elapsed_time
 
 @app.route("/commit_time/<int:t>")
 def commit_time(t):
-    flask.session['commit_time'] = t
-    return "Commit time set to %d." % (t,)
+    with glob:
+        glob["commit_time"] = t
+        return "Commit time set to: %d seconds" % glob["commit_time"]
 
 @app.route("/get_commit_time")
 def get_commit_time():
-    if 'commit_time' in flask.session:
-        return "Last committed time: %d." % (flask.session['commit_time'],)
-    return 'No commit time specified.'
-
-@app.route("/jsontest")
-def jsontest():
-    return flask.jsonify(username="admin", age=42, sex=["male"])
+    with glob:
+        if "commit_time" in glob:
+            return "Last commit time: %d seconds" % glob["commit_time"]
+        else:
+            return "No commit time has been specified yet."
 
 if __name__ == "__main__":
-    app.secret_key = 'super random'
-    app.debug = True
-    app.run(host="0.0.0.0")
+    app.secret_key = "replace me!"
+    app.run(debug = True)
 
---
+++ util.py
@@ -0,0 +1,126 @@
+
+class UnlockedAccessError(Exception):
+    """
+    Thrown when someone tries to acces the dictionary while it
+    is unlocked.
+    """
+    
+    pass
+
+class ThreadedDataStore:
+    def __init__(self):
+        """
+        Create an object that behaves as a standard dictionary,
+        but is thread-safe.
+        """
+        
+        try:
+            import threading
+        except ImportError:
+            import dummy_threading as threading
+        
+        # the dict we slap thread safety onto
+        self.__dict = {}
+        self.__default = None
+        
+        self.__lock = threading.RLock()
+        self.__locked = False
+    
+    def __enter__(self):
+        self.__lock.acquire()
+        self.__locked = True
+    
+    def __exit__(self, typ = None, val = None, trace = None):
+        self.__lock.release()
+        self.__locked = False
+    
+    def __contains__(self, key):
+        """
+        Does the data store contain the given key?
+        """
+
+        if not self.__locked:
+            raise UnlockedAccessError("Data store unlocked; access denied.")
+        
+        return key in self.__dict
+    
+    def __getitem__(self, key):
+        """
+        Returns the item in the store at the time of access.
+        """
+
+        if not self.__locked:
+            raise UnlockedAccessError("Data store unlocked; access denied.")
+        
+        return self.__dict[key]
+    
+    def __len__(self):
+        """
+        Returns the number of items in the store.
+        """
+        
+        if not self.__locked:
+            raise UnlockedAccessError("Data store unlocked; access denied.")
+        
+        return len(self.__data)
+
+    def __setitem__(self, key, value):
+        """
+        Changes the item in the store to whatever is specified.
+        """
+        
+        if not self.__locked:
+            raise UnlockedAccessError("Data store unlocked; access denied.")
+        
+        self.__dict[key] = value
+    
+    def __sizeof__(self):
+        """
+        Returns the number of items in the store.
+        """
+        
+        if not self.__locked:
+            raise UnlockedAccessError("Data store unlocked; access denied.")
+        
+        return self.__dict.__sizeof__()
+    
+    def has_key(self, key):
+        """
+        Does the store contain the given key?
+        """
+        
+        if not self.__locked:
+            raise UnlockedAccessError("Data store unlocked; access denied.")
+        
+        return self.__contains__(key)
+    
+    def items(self):
+        """
+        Returns a copy of all the (key, value) pairs at the time of
+        access.
+        """
+        
+        if not self.__locked:
+            raise UnlockedAccessError("Data store unlocked; access denied.")
+        
+        return self.__dict.items()
+    
+    def keys(self):
+        """
+        Returns all the keys in the store.
+        """
+        
+        if not self.__locked:
+            raise UnlockedAccessError("Data store unlocked; access denied.")
+        
+        return self.__dict.keys()
+    
+    def values(self):
+        """
+        Returns all the values in the store.
+        """
+        
+        if not self.__locked:
+            raise UnlockedAccessError("Data store unlocked; access denied.")
+        
+        return self.__dict.values()

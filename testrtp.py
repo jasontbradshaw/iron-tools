5125155e5a0e59e62507705f869c5499c0798c47
Elben Shira <elbenshira@gmail.com>
2010-06-08 00:03:24 -0500

    nicer play-dump-play test interface; removes existing outfile

--- testrtp.py
+++ testrtp.py
@@ -1,8 +1,8 @@
 import os
+import os.path
 import rtp
 import subprocess as sp
 
-# TODO make this a real test...
 
 VLCCMD_OSX = '/Applications/VLC.app/Contents/MacOS/VLC'
 VLCCMD_LINUX = 'vlc'
@@ -12,9 +12,16 @@ if os.uname()[0] == 'Darwin':
 elif os.uname()[0] == 'Linux':
     VLCCMD = VLCCMD_LINUX
 
-def test(infile='night.tsdump', outfile='playdumpplaytest.tsdump'):
+def playdumpplay_test(infile='night.tsdump', outfile='playdumpplaytest.tsdump'):
     print 'begin play-dump-play test.'
 
+    if not os.path.isfile(infile):
+        print "infile '%s' does not exist. done." % infile
+    if os.path.isfile(outfile):
+        print "outfile '%s' exists; removing." % outfile
+        os.remove(outfile)
+
+    DEVNULL = open('/dev/null', 'w')
     play = rtp.RTPPlay('localhost', 9876, infile)
     dump = rtp.RTPDump('localhost', 9876, outfile)
     play2 = rtp.RTPPlay('localhost', 9000, outfile)
@@ -33,10 +40,10 @@ def test(infile='night.tsdump', outfile='playdumpplaytest.tsdump'):
     print "press [enter] to start 'vlc'"
     raw_input()
     args = [VLCCMD, 'rtp://@:9000']
-    vlc = sp.Popen(args)
+    vlc = sp.Popen(args, stderr=DEVNULL, stdout=DEVNULL)
 
     while True:
-        print "press 'q' and [enter] to quit"
+        print "[enter] to view processes; 'q' to quit"
         i = raw_input()
         if i == 'q':
             break
@@ -48,11 +55,16 @@ def test(infile='night.tsdump', outfile='playdumpplaytest.tsdump'):
     play.stop()
     dump.stop()
     play2.stop()
+    vlc.kill()
+    print "killed processes"
+    print "press [enter] to end"
+    raw_input()
 
     print 'play  alive:', play.isalive()
     print 'dump  alive:', dump.isalive()
     print 'play2 alive:', play2.isalive()
-    print 'done'
+    print 'vlc   alive:', (vlc.poll() is None) # is vlc alive?
+    print 'done.'
 
 if __name__ == "__main__":
-    test()
+    playdumpplay_test()

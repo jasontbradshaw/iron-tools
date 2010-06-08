97a59880c71b2f3356db35117a19ccfa0379c1b3
Elben Shira <elbenshira@gmail.com>
2010-06-07 20:54:07 -0500

    rtp play semi-working

--- rtp.py
+++ rtp.py
@@ -1,6 +1,48 @@
 import subprocess as sp
 
-class RTPDump:
+class RTPTools:
+    def __init__(self):
+        self.proc = None
+
+    def isalive(self):
+        return self.proc.poll() is None
+
+    def start(self):
+        pass
+
+    def stop(self):
+        self.proc.terminate()
+
+    def pid(self):
+        return self.proc.pid
+
+class RTPPlay(RTPTools):
+    def __init__(self, address, port, inputfile=None, begintime=0,
+            path='rtpplay'):
+        self.proc = None
+
+        self.address = address
+        self.port = port
+        self.inputfile = inputfile
+        self.begintime = begintime
+        self.path = path
+
+    def start(self):
+        if not self.proc:
+            args = ["./%s" % self.path,
+                    "-f", self.inputfile,
+                    "-b", str(self.begintime),
+                    "-v",
+                    "%s/%d" % (self.address, self.port),
+                    "1>", "/dev/null",    # no stdout
+                    "2>", "/dev/null",    # no stderr
+                    ]
+
+            self.proc = sp.Popen(args)
+        return self.pid()
+
+
+class RTPDump(RTPTools):
     # TODO:
     # - allow user to append to existing file
     # - backup files
@@ -14,25 +56,18 @@ class RTPDump:
 
         self.address = address
         self.port = port
-        self.path = path
         self.dumpformat = dumpformat
         self.outputfile = outputfile
+        self.path = path
 
     def start(self):
-        args = ["./%s" % self.path,
-                "-F", self.dumpformat]
-        if self.outputfile:
-            args.extend(["-o", self.outputfile])
-        args.append("%s/%d" % (self.address, self.port))
+        if not self.proc:
+            args = ["./%s" % self.path,
+                    "-F", self.dumpformat]
+            if self.outputfile:
+                args.extend(["-o", self.outputfile])
+            args.append("%s/%d" % (self.address, self.port))
 
-        self.proc = sp.Popen(args)
+            self.proc = sp.Popen(args)
         return self.pid()
-    
-    def isalive(self):
-        return self.proc.poll() is None
 
-    def stop(self):
-        self.proc.terminate()
-
-    def pid(self):
-        return self.proc.pid

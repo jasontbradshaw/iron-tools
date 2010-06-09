3caa2b64e260162f6ca605e21541a7e809c602a6
Paul <paul@paul-laptop.(none)>
2010-06-08 19:14:38 -0500

    created basic starting template

---
+++ receiverweb.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import flask
+from flask import Flask
+from flask import flash
+
+import util
+
+app = Flask(__name__)
+glob = util.ThreadedDataStore()
+
+@app.route("/")
+def hello():
+    return "Receiver Web"
+
+# create play
+@app.route("/play")
+def play():
+    return null
+
+# Creat stop
+@app.route("/stop")
+def stop():
+    return null
+
+if __name__ == "__main__":
+    app.run(debug = True)

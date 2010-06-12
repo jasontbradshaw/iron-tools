7d68e16758d0e23b0a02d195473a4f3d082b1c33
Elben Shira <elbenshira@gmail.com>
2010-06-11 20:32:41 -0500

    more basic tests

--- collectorweb_tests.py
+++ collectorweb_tests.py
@@ -9,9 +9,27 @@ class CollectorWebTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_start_record(self):
-        rv = self.app.get('/start_record')
+    def test_test(self):
+        rv = self.app.get('/')
+        assert 'Collector Web' in rv.data
+        rv = self.app.get('/stop_record')
         assert '500' not in rv.data
+
+    def test_stop_record1(self):
+        rv = self.app.get('/stop_record')
+        assert '{}' in rv.data
+
+    def test_record(self):
+        rv = self.app.get('/stop_record')
+        print rv.data
+        assert '{}' in rv.data
+
+        rv = self.app.get('/start_record')
+        print rv
+        assert '{}' in rv.data
+
+        rv = self.app.get('/stop_record')
+        assert '{}' in rv.data
 
         # stop_record with no rtpdump running should return '{}'
         rv = self.app.get('/stop_record')

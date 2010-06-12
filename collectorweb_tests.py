fa58153b05594d578d8f6e351b708753a0bc8a7f
Elben Shira <elbenshira@gmail.com>
2010-06-11 20:37:34 -0500

    simpler tests

--- collectorweb_tests.py
+++ collectorweb_tests.py
@@ -21,39 +21,16 @@ class CollectorWebTestCase(unittest.TestCase):
 
     def test_record(self):
         rv = self.app.get('/stop_record')
-        print rv.data
         assert '{}' in rv.data
 
         rv = self.app.get('/start_record')
-        print rv
         assert '{}' in rv.data
-
-        rv = self.app.get('/stop_record')
-        assert '{}' in rv.data
-
-        # stop_record with no rtpdump running should return '{}'
-        rv = self.app.get('/stop_record')
-        assert '500' not in rv.data
-        assert '{}' in rv.data
-        js = json.loads(rv.data)
-        assert len(js) == 0
-
-        # a successful rtpdump start returns '{}'
-        rv = self.app.get('/start_record')
-        assert '500' not in rv.data
-        assert '{}' in rv.data
-        js = json.loads(rv.data)
-        assert len(js) == 0
 
         rv = self.app.get('/start_record')
-        assert '500' not in rv.data
-        js = json.loads(rv.data)
-        assert js['warning'] == 'rtpplay already running.'
+        assert 'error' in rv.data
 
         rv = self.app.get('/stop_record')
-        js = json.loads(rv.data)
-        assert rv.data == '{}'
-        assert len(js) == 0
+        assert '{}' in rv.data
 
 if __name__ == '__main__':
     unittest.main()

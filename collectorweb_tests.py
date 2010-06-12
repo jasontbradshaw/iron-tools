76923056f4b722a7cd67307e83eebcf5caa692a5
Elben Shira <elbenshira@gmail.com>
2010-06-11 20:49:31 -0500

    timing tests

--- collectorweb_tests.py
+++ collectorweb_tests.py
@@ -1,6 +1,7 @@
 import collectorweb
 import unittest
 import json
+import time
 
 class CollectorWebTestCase(unittest.TestCase):
     def setUp(self):
@@ -28,9 +29,33 @@ class CollectorWebTestCase(unittest.TestCase):
 
         rv = self.app.get('/start_record')
         assert 'warning' in rv.data
+        js = json.loads(rv.data)
+        assert 'warning' in js
 
         rv = self.app.get('/stop_record')
         assert '{}' in rv.data
+
+    def test_elapsed_time(self):
+        rv = self.app.get('/get_elapsed_time')
+        assert 'null' in rv.data
+        js = json.loads(rv.data)
+        assert 'elapsed_time' in js
+
+        rv = self.app.get('/start_record')
+        assert '{}' in rv.data
+
+        rv = self.app.get('/get_elapsed_time')
+        js = json.loads(rv.data)
+        assert 'elapsed_time' in js
+        t1 = int(js['elapsed_time'])
+
+        time.sleep(3)
+
+        rv = self.app.get('/get_elapsed_time')
+        js = json.loads(rv.data)
+        assert 'elapsed_time' in js
+        t2 = int(js['elapsed_time'])
+        assert 3 <= t2 - t1 <= 4    # close enough
 
 if __name__ == '__main__':
     unittest.main()

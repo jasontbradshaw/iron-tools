2fd4016d6fee4a73bcd5bda5774ea652f793b974
Paul <paul@paul-laptop.(none)>
2010-06-14 22:05:04 -0500

    added a few simple test cases to receiverweb_test.py

--- receiverweb_tests.py
+++ receiverweb_tests.py
@@ -11,9 +11,19 @@ class ReceiverWebTestCase(unittest.TestCase):
     def setUp(self):
         self.app = receiverweb.app.test_client()
 
-    def test_test(self):
+    def test_home(self):
         rv = self.app.get('/')
         assert 'Receiver Web' in rv.data
+
+    # if there is nothing to stop it should return {}
+    def test_stop(self):
+        rv = self.app.get('/stop')
+        assert '{}' in rv.data
+
+    # even when the directory is empty it will not return {}
+    def test_get_file_list(self):
+        rv = self.app.get('/get_file_list')
+        assert '{}' not in rv.data
 
 if __name__ == '__main__':
     unittest.main()

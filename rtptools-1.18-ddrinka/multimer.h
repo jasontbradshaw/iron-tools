93ab11f8f7de5cbe50c8c8fc5d6ece8598a027a2
Jesse <advisor@ubuntu.ubuntu-domain>
2010-06-15 21:24:15 -0500

    removed built version of rtptools source

--- rtptools-1.18-ddrinka/multimer.h
+++
@@ -1,4 +0,0 @@
-extern struct timeval *timer_set(struct timeval *interval, 
-  Notify_func func, Notify_client client, int relative);
-extern struct timeval *timer_get(struct timeval *timeout);
-extern int timer_pending(void);

43784876617ac5c1e74a655867b7f75cc3a819f0
Paul <paul@paul-laptop.(none)>
2010-06-15 21:58:46 -0500

    added test cases to receiverweb_tests.py

--- receiverweb_tests.py
+++ receiverweb_tests.py
@@ -2,6 +2,7 @@ import receiverweb
 import unittest
 import json
 import time
+import re
 
 # before running create
 # /collector/dump
@@ -15,15 +16,46 @@ class ReceiverWebTestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert 'Receiver Web' in rv.data
 
-    # if there is nothing to stop it should return {}
+    # assert that stop does nothing if nothing is playing
     def test_stop(self):
         rv = self.app.get('/stop')
         assert '{}' in rv.data
 
-    # even when the directory is empty it will not return {}
+    # assert nothing is in the list if the directory is empty
     def test_get_file_list(self):
         rv = self.app.get('/get_file_list')
-        assert '{}' not in rv.data
+        js = json.loads(rv.data)
+        assert 'file_list' in js
+        assert js['file_list'] == []
+
+    def test_commit_time_1(self):
+        rv = self.app.get('/commit_time')
+        js = json.loads(rv.data)
+        assert 'commit_time' in js
+
+    # if the directory is empty assert commit_time = 0
+    def test_commit_time_2(self):
+        rv = self.app.get('/commit_time')
+        js = json.loads(rv.data)
+
+        rv2 = self.app.get('/get_file_list')
+        js2 = json.loads(rv2.data)
+        if js2['file_list'] == []:
+            assert int(js['commit_time']) == 0
+        else:
+            assert 'commit_time' in js
+
+    # asserts that fake_test.dump did not exist and proper error was returned
+    def test_play_file(self):
+        rv = self.app.get('/play_file/fake_test.dump')
+        js = json.loads(rv.data)
+
+        rv2 = self.app.get('/get_file_list')
+        js2 = json.loads(rv2.data)
+        if js2['file_list'] == []:
+            assert js['error'] == "Could not find file 'fake_test.dump'."
+        else:
+            assert 'commit_time' in js
 
 if __name__ == '__main__':
     unittest.main()

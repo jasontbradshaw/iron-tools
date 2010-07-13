b773ee3dcc00aff5ba997f13b556801cc5a74da6
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2010-07-13 18:47:01 -0500

    removed RTPDump armed file remoaval and replaced it with simpler, safer modification time checking all around.  refactored rtp.py somewhat, clarifying docstrings and maximizing boolean zen.

--- rtp.py
+++ rtp.py
@@ -9,40 +9,42 @@ class RTPTools:
         self.lock = threading.Lock()
 
     def isalive(self):
-        if self.proc is None:
-            return False
-        return self.proc.poll() is None
+        """
+        Checks if the process is non-'None', and if so returns its
+        poll status.
+        """
+        
+        return self.proc and self.proc.poll() is None
 
     def start(self):
-        raise NotImplementedError("start not implemented")
+        raise NotImplementedError("'start' method not implemented")
 
     def stop(self):
         """
         Kills the process launched by 'start'.
         """
         
-        if not self.isalive():
-            return
-        self.proc.terminate()
+        if self.isalive():
+            self.proc.terminate()
 
     def pid(self):
         """
         Returns the process identifier of the process started by 'start'.
         """
         
-        if not self.proc:
-            return None
-        return self.proc.pid
+        if self.proc:
+            return self.proc.pid
+        return None
 
 class RTPPlay(RTPTools):
     """
     Streams an RTP dump file to a network address.
     """
     
-    def __init__(self, path='rtpplay'):
+    def __init__(self, path="rtpplay", armed_file_name="armed"):
         """
         Builds an RTPPlay object, optionally with the path to the binary
-        rtpplay file.
+        rtpplay file and the name of the armed flag file.
         """
 
         # init parent for its precious methodly fluids
@@ -52,35 +54,21 @@ class RTPPlay(RTPTools):
         self.path = path
         self.proc = None
         
-        # the time the process was started
+        # the time the process was started by 'start'
         self.proc_start_time = None
         
         # the file where the armed status should exist
         self.armed_file = os.path.join(os.path.dirname(self.path),
-                                       armed_dir, "armed")
+                                       armed_dir, armed_file_name)
         
-        # clean up any previously existing armed file
-        self.remove_armed_file()
-        
-    def remove_armed_file(self):
-        """
-        Removes the 'armed' file if it exists as specified.  Returns 'True'
-        if the file was removed, 'False' otherwise.
-        """
-        
-        try:
-            os.remove(self.armed_file)
-            return True
-        except:
-            return False
-    
     def start(self, inputfile, address, port, start_time=None, end_time=None,
               wait_start=False):
         """
-        Start the rtpplay process with a file to play, an address, then port
+        Start the rtpplay process with a file to play, an address, a port
         to play to, an optional time to start playing the stream from, and
         whether to pause execution and wait after seeking to the start time.
-        If wait_start is True, a call to begin_playback will be neccessary
+        
+        If wait_start is True, a call to 'begin_playback' will be neccessary
         to actually start the playback.
         """
         
@@ -103,16 +91,17 @@ class RTPPlay(RTPTools):
                 if wait_start:
                     args.append("-w")
                 
+                # add address/port string
                 args.extend(["%s/%d" % (address, port)])
                 
+                # mark the time we're starting the process at
+                self.proc_start_time = time.time()
+        
                 # TODO: close devnull?
                 DEVNULL = open(os.devnull, 'w')
                 self.proc = sp.Popen(args, stderr=DEVNULL, stdout=DEVNULL,
                                      stdin=sp.PIPE)
                 
-                # mark the time we started the process
-                self.proc_start_time = time.time()
-        
         return self.pid()
     
     def stop(self):
@@ -120,17 +109,13 @@ class RTPPlay(RTPTools):
         Kills the process launched by 'start'.
         """
         
-        if not self.isalive():
-            return
+        # only kill the process if it's currently alive
+        if self.isalive():
+            self.proc.terminate()
+            
+            # remove last start time since we were just stopped
+            self.proc_start_time = None
         
-        self.proc.terminate()
-        
-        # remove last start time since we were just stopped
-        self.proc_start_time = None
-        
-        # clean up armed file
-        self.remove_armed_file()
-
     def begin_playback(self):
         """
         If rtpplay was started with waiting turned on, this sends the command
@@ -146,18 +131,13 @@ class RTPPlay(RTPTools):
         """
         Determines if the currently armed file is done being armed.  Returns
         'True' if this is the case, and 'False' if nothing has been played
-        or the file can't be found.
+        yet or the file can't be found.
         """
         
-        # no start time or file doesn't exist
-        if self.proc_start_time is None or not os.path.exists(self.armed_file):
-            return False
-        # modification time of file is before most recent start time
-        elif os.path.getmtime(self.armed_file) < self.proc_start_time:
-            return False
-        
-        # otherwise, modification time was after we started the process
-        return True
+        # the file exists and its modification time is after the start time
+        # NOTE: any time is greater than or equal to 'None'
+        return (os.path.exists(self.armed_file) and
+                os.path.getmtime(self.armed_file) >= self.proc_start_time):
     
 class RTPDump(RTPTools):
     """

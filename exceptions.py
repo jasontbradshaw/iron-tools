641785543f00d87d7d9f62bbc1402a1994e6b2a1
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2010-08-02 19:55:43 -0500

    added forgotten files

---
+++ exceptions.py
@@ -0,0 +1,20 @@
+
+class ProcessAlreadyRunningError(Exception):
+    """
+    Indicates an attempt to start the same process twice.
+    """
+
+class ProcessOperationTimeoutError(Exception):
+    """
+    Indicates a process operation that timed out.
+    """
+
+class NoRecordedFileError(Exception):
+    """
+    Indicates an attempt to commit a time without a previous recording.
+    """
+
+class FileNotFoundError(Exception):
+    """
+    Indicates that a file could not be found.
+    """

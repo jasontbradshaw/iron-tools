2ff272995f1a945c4141d9572909884aba9a7182
root <root@paul.(none)>
2010-08-23 22:25:12 -0500

    Adjusted ports/IPs for sane production use

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ if ENVIRONMENT == ENV_DEV:
 
 elif ENVIRONMENT == ENV_PRODUCTION:
     #Playback config
-    RTPPLAY_ADDRESS = "127.0.0.1"
+    RTPPLAY_ADDRESS = "10.97.0.81"
     RTPPLAY_PORT = 5008
 
     #Recorder config
@@ -55,8 +55,11 @@ elif ENVIRONMENT == ENV_PRODUCTION:
     RTPDUMP_PREVIEW_ADDRESS = "10.98.0.81"
     RTPDUMP_PREVIEW_PORT = 5008
 
-    SYNC_DIR = "/video"
+    SYNC_DIR = "/video/hires/test1"
     DUMP_DIR = os.path.join(SYNC_DIR, "dump")
+
+    RECORDER_PORT = 80
+    PLAYBACK_PORT = 80
 
 else:
     raise NotImplemented("'%s' not a supported environment." % ENVIRONMENT)
